4c87b614c9c09e391c7d3533a986c2b06e743d8f
akhterjawad <akhterjawad566@gmail.com>
2024-06-04 13:31:58 +0500

    arrow function

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-// arrow function
+// // arrow function
 
-let a = arrow = item => {
-    console.log(`hello`, item);
-    // return `hello`,item
+// let a = arrow = item => {
+//     console.log(`hello`, item);
+//     // return `hello`,item
 
-}
-console.log(arrow('haseeb'));
-arrow(`jawad`)
+// }
+// console.log(arrow('haseeb'));
+// arrow(`jawad`)
 
-let value = 1000;
-let main = new Promise((resolve, reject) => {
-    if (value >= 1001) {
-        console.log('pass');
-        resolve()
-    } else {
-        console.log('fail');
-        reject()
-    }
-})
-    .then(() => {
-        console.log('then');
-    })
-    .catch(() => {
-        console.log('catch');
-    })
-jawad('ahmed')
-    function jawad(jj) {
-        console.log(`haseeb ${jj}`);
-    }
-
-    function hasnain (ok){
-        
-        return `vip ${ok}`;
-    }
-    let ahmed = hasnain(`kamran`)
-    console.log(ahmed);
-
-//      nana=(chacha)=>{
-// console.log(`nana ${chacha} nani`);
+// let value = 1000;
+// let main = new Promise((resolve, reject) => {
+//     if (value >= 1001) {
+//         console.log('pass');
+//         resolve()
+//     } else {
+//         console.log('fail');
+//         reject()
 //     }
-//     nana('nawaisa')
-lemon =(smit)=>{
-    console.log(`JS${smit} `);
-}
-lemon(` practice`)
+// })
+//     .then(() => {
+//         console.log('then');
+//     })
+//     .catch(() => {
+//         console.log('catch');
+//     })
+// jawad('ahmed')
+//     function jawad(jj) {
+//         console.log(`haseeb ${jj}`);
+//     }
+
+//     function hasnain (ok){
+
+//         return `vip ${ok}`;
+//     }
+//     let ahmed = hasnain(`kamran`)
+//     console.log(ahmed);
+
+// //      nana=(chacha)=>{
+// // console.log(`nana ${chacha} nani`);
+// //     }
+// //     nana('nawaisa')
+// lemon =(smit)=>{
+//     console.log(`JS${smit} `);
+// }
+// lemon(` practice`)
+jawad=()=>{ return 'kamran' }
+console.log(jawad);
