818f24396b39fb7809f74d29d229cdc95bf33bbe
akhterjawad <akhterjawad566@gmail.com>
2024-08-03 08:30:55 -0700

    half

--- phone app/app.js
+++ phone app/app.js
@@ -2,6 +2,7 @@ const phones = [
     {
         brand: 'Samsung ',
         model: 'Galaxy A55',
+        quantity: 0,
         ram: 12,
         rom: 256,
         camera: '50 megapixel',
@@ -11,6 +12,7 @@ const phones = [
     {
         brand: 'Apple ',
         model: 'iPhone XS Max',
+        quantity: 0,
         ram: 4,
         rom: 128,
         camera: '12 megapixel',
@@ -20,6 +22,7 @@ const phones = [
     {
         brand: 'Apple ',
         model: 'iPhone 11',
+        quantity: 0,
         ram: 4,
         rom: 128,
         camera: '12 megapixel',
@@ -29,6 +32,7 @@ const phones = [
     {
         brand: 'Samsung ',
         model: 'Galaxy S24 Ultra',
+        quantity: 0,
         ram: 12,
         rom: 512,
         camera: '200 megapixel',
@@ -38,6 +42,7 @@ const phones = [
     {
         brand: 'Sony ',
         model: 'Xperia 1V',
+        quantity: 0,
         ram: 12,
         rom: 256,
         camera: '48 megapixel',
@@ -47,6 +52,7 @@ const phones = [
     {
         brand: 'Xiaomi ',
         model: 'Poco F5',
+        quantity: 0,
         ram: 8,
         rom: 256,
         camera: '48 megapixel',
@@ -56,6 +62,7 @@ const phones = [
     {
         brand: 'Google ',
         model: 'Pixel 8 Pro',
+        quantity: 0,
         ram: 12,
         rom: 512,
         camera: '50 megapixel',
@@ -65,6 +72,7 @@ const phones = [
     {
         brand: 'Xiaomi ',
         model: 'Redmi Note 13 Pro+',
+        quantity: 0,
         ram: 16,
         rom: 512,
         camera: '108 megapixel',
@@ -72,11 +80,11 @@ const phones = [
         img: 'https://fdn2.gsmarena.com/vv/bigpic/xiaomi-redmi-note-13-pro-plus-int.jpg'
     },
 ]
-let div =document.querySelector('.products')
-let button =document.querySelector('.button')
+let div = document.querySelector('.products')
+let button = document.querySelector('.button')
 
-phones.map((item,index)=>{
-div.innerHTML+=`
+phones.map((item, index) => {
+    div.innerHTML += `
     <div class="rounded border card-width  p-2 ">
                     <div class=" text-center  p-2"><img src="${item.img}" class="card-img-width "
                             alt="no image">
@@ -96,6 +104,21 @@ div.innerHTML+=`
                 </div>
 `
 });
-button.addEventListener('click',()=>{
-window.location='index-2.html'    
+
+let array = [];
+function addtocard(card) {
+    if (array.includes(phones[card])) {
+        phones[card].quantity += 1
+    } else {
+        phones[card].quantity += 1
+        array.push(phones[card])
+    }
+    console.log(array);
+
+}
+
+button.addEventListener('click', () => {
+    console.log(`index-2.html`);
+localStorage.setItem(`SendData`,JSON.stringify(array))
+    window.location = 'index-2.html'
 })
